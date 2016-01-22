72dcae991178c8862847a6c389eafbe9016bef69
Fabio Matias <fabio.matias@closer.pt>
2016-01-22 18:16:45 +0000

    fix compile

--- MoToolsMVC/Controllers/TeamController.cs
+++ MoToolsMVC/Controllers/TeamController.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Blob;
+﻿//using Microsoft.WindowsAzure.Storage;
+//using Microsoft.WindowsAzure.Storage.Blob;
 using MoToolsMVC.BLL;
 using MoToolsMVC.BLL.CaseDDLOptions;
 using System;
